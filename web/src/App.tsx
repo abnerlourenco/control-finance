7012ff1a81b05145f569aa5fb12af3534e4cc5f4
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2023-01-27 21:24:29 -0300

    components init

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,8 @@
+import { Header } from "./components/Header";
 
 
-function App() {
-  return <h1>Hello World!!!</h1>
+export default function App() {
+  return (
+    <Header />
+  )
 }
-
-export default App
---
+++ web/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+
+
+export function Header() {
+    return (
+        <header className="header">
+            <a href="/">
+                <img src="logo.svg" alt="Logo Control Finances" />
+            </a>
+            
+            <div>
+                
+            </div>
+
+        </header>
+    )
+}
