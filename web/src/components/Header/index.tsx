3f40034f0b1256504ff2fd8237eed5bc243ce281
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2023-01-30 14:13:43 -0300

    changes colors and logo

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -4,17 +4,8 @@ export function Header() {
     return (
         <header className="header">
             <a href="/">
-                <img className="logo" src="logo.svg" alt="Logo" />
+                <img className="logo" src="logo-cinza.svg" alt="Logo" />
             </a>
-            
-            <div className="navbar">
-                <div>
-                    <a href="/">Geral</a>
-                </div>
-                <div>
-                    <a href="/">Dashboards</a>
-                </div>
-            </div>
 
             <div className="perfil">
                 <img className="img-perfil" src="perfil.jpg" alt="" />
