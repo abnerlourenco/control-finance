3b6c900a333bfad144f271f3055ab5b045b872c7
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2023-01-31 20:37:05 -0300

    Add component Card Propos and style

--- web/src/components/Card/index.tsx
+++ web/src/components/Card/index.tsx
@@ -1,5 +1,19 @@
 import "./styles.css"
 
-export function Card() {
-    
+interface CardProps {
+    operationType: string;
+    imagePath: string;
+}
+
+export function Card(props: CardProps) {
+   return ( 
+    <div className="Card-container">
+        <h3>
+            <span>{props.operationType}</span>
+            <img src={props.imagePath} alt="Imagem de saídas" />
+        </h3>
+
+        <p id="expenseDisplay">R$ 0,00</p>
+    </div>
+   )
 }
